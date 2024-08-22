22108c150a984b0c78cc445238bab93ad6a2eadf
Carlos Vinicius <131323480+Vviniciuss13@users.noreply.github.com>
2024-08-22 19:03:06 +0000

    first_commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cicd_angular_vercel',
+  webDir: 'www'
+};
+
+export default config;
